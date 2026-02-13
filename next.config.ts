29af52dd21d5d54ac7efe979b39f3b5e571faf31
DeepStatistix <aqibgul@skuastkashmir.ac.in>
2026-02-13 17:17:37 +0530

    Rebuild static export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/aqib-portfolio",
+  assetPrefix: "/aqib-portfolio/",
 };
 
 export default nextConfig;
