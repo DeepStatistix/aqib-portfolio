b775636459d9641d8bc754489b3f3172f8cce665
DeepStatistix <aqibgul@skuastkashmir.ac.in>
2026-02-13 17:32:43 +0530

    Switch to docs deployment

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: "export",
   basePath: "/aqib-portfolio",
   assetPrefix: "/aqib-portfolio/",
 };
 
-export default nextConfig;
+module.exports = nextConfig;
